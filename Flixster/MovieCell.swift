3ffc6f30d376d5a41a40237be241864558b58150
Alex Trejo <hatrejogalvan18@students.desu.edu>
2020-01-26 23:25:30 -0500

    Finished Flixster

---
+++ Flixster/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  Flixster
+//
+//  Created by Alex Trejo on 1/26/20.
+//  Copyright © 2020 Alex Trejo. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
