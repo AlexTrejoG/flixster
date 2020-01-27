773c5273aee24ed84cb08826c1d4591932026f26
Alex Trejo <hatrejogalvan18@students.desu.edu>
2020-01-26 21:24:48 -0500

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Alex Trejo on 1/23/20.
+//  Copyright © 2020 Alex Trejo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
