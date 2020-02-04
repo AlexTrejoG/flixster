79104efa81240fb0dc06884f982d6b6170f91a2d
Alex Trejo <hatrejogalvan18@students.desu.edu>
2020-02-04 16:24:43 -0500

    Flixster +

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Alex Trejo on 2/4/20.
+//  Copyright © 2020 Alex Trejo. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
